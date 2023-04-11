609e7759c3e436a51413b583c7487b834ef37b42
henrietei <113431841+henrietei@users.noreply.github.com>
2023-04-11 14:20:22 +0300

    labojums

--- main.py
+++ main.py
@@ -15,19 +15,10 @@ def draw_rect(x,y):
     pygame.draw.rect(screen, green, pygame.Rect(x, y, 60, 60))
     pygame.display.flip()
 
-
-
-def surface():
-    pygame.draw.rect(screen, gray1, (100, 105, 60, 90))
-    pygame.draw.rect(screen, gray1, (185, 105, 60, 90))
-    pygame.draw.rect(screen, gray1, (270, 105, 60, 90))
-    pygame.draw.rect(screen, gray1, (355, 105, 60, 90))
-    pygame.draw.rect(screen, gray1, (440, 105, 60, 90))
-    pygame.draw.rect(screen, gray1, (100, 205, 405 , 90))
-    pygame.draw.rect(screen, gray1, (100, 305, 405, 90))
-    pygame.draw.rect(screen, gray1, (100, 405, 405, 90))
-    pygame.draw.rect(screen, gray1, (100, 505, 405, 90))
-#pygame.draw.rect(screen, color, (100, 610, 225, 72))
+def surface1():
+    for i in range (5):
+        for i1 in range (6):
+            pygame.draw.rect(screen, gray1, (100+i*85, 105+100*i1, 60, 90))
 
 
 with open('words.txt', 'r',  encoding="utf-8") as g:
@@ -44,18 +35,15 @@ pygame.init()
 
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode([600, 700])
+screen = pygame.display.set_mode([600, 800])
 base_font = pygame.font.Font("SourceCodePro-ExtraBold.ttf", 72)
 user_text = ''
-input_rect = pygame.Rect(100, 95, 225, 72)
-  
 
   
-active = False
 
-screen.fill(gray)
 
-surface()
+
+surface1()
 while True:
     for event in pygame.event.get():
       # if user types QUIT then the screen will close
@@ -66,56 +54,30 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
                 user_text = user_text[:-2]
-                print("kgghh")
-                screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+                #print("kgghh")
+                
+                
                 
 
             else:
                 if len(user_text)<=9:
                     if event.unicode != "'":
                         user_text += event.unicode + " " 
+
+        if event.type==pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            user_text1=user_text.replace(" ", "")
+            print(user_text1)
+                
+
                 
       
-    # it will set background color of screen
-    
-  
-    """if active:
-        color = color_active
-    else:
-        color = color_passive"""
-          
-    # draw rectangle and argument passed which should
-    # be on screen
-    """pygame.draw.rect(screen, gray1, (100, 105, 405, 90))
-    pygame.draw.rect(screen, gray1, (100, 205, 405 , 90))
-    pygame.draw.rect(screen, gray1, (100, 305, 405, 90))
-    pygame.draw.rect(screen, gray1, (100, 405, 405, 90))
-    pygame.draw.rect(screen, gray1, (100, 505, 405, 90))
-    #pygame.draw.rect(screen, color, (100, 610, 225, 72))"""
-  
+    screen.fill(gray)
+    surface1()
+    input_rect = pygame.Rect(100, 95, 225, 72)
     text_surface = base_font.render(user_text, True, black)
-    #pygame.draw.rect(screen, black, input_rect)
-      
-    # render at position stated in arguments
     screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
-    
-    """if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_RETURN:  
-           print(user_text)
-           time.sleep(1)
-           for i in range (5):
-                if user_text.find(a[i])== -1:
-                    
-                    print("nav")
-                if user_text.find(a[i]) != -1: 
-                    print(i, "ir")
-                    #diceDisplay = base_font.render(str(user_text[i]), 1, black)
-                    #screen.blit(diceDisplay, (520, 30))
-                    draw_rect(i, i+100)
-"""
-               
-               
-
+    pygame.display.flip()
+    clock.tick(60)
 
 
     
@@ -125,15 +87,3 @@ while True:
       
   
       
-    # set width of textfield so that text cannot get
-    # outside of user's text input
-    #input_rect.w = max(100, 155)
-      
-    # display.flip() will update only a portion of the
-    # screen to updated, not full area
-    pygame.display.flip()
-      
-    # clock.tick(60) means that for every second at most
-    # 60 frames should be passed.
-    clock.tick(60)
-    
